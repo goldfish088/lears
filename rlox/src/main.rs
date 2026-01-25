e0f091ad4c012da48689195573239a7ba0c7a602
Abraham <ae248abraham@gmail.com>
2026-01-25 22:44:53 +1100

    lox: [WIP] basic scanner implementation

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,12 +1,63 @@
 use std::env;
 use std::process;
+use std::fs;
+use std::io::{self, Write};
+use std::vec;
 
-mod scanner {
+#[allow(dead_code)]
+fn print_type<T>(_: &T) {
+    println!("&type = {}", std::any::type_name::<&T>());
 }
 
-// fn print_type<T>(_: &T) {
-//     println!("type is: {}", std::any::type_name::<T>());
-// }
+struct Token {
+
+}
+
+fn scan_tokens() -> Vec<Token> {
+    Vec::new::<>()
+}
+
+fn interpret(code: &str) {
+    println!("length = {}", code.len());
+    println!("code = {}", code);
+    todo!();
+}
+
+fn run_file(path: &String) {
+    // if code.is_err() {
+    //     eprintln!("oops something went wrong");
+    //     process::exit(42);
+    // }
+    let code = fs::read_to_string(path).unwrap_or_else(|_| {
+        eprintln!("oops something went wrong");
+        process::exit(42);
+    });
+
+    print_type(&code);
+    interpret(&code);
+}
+
+fn run_repl() {
+    // init scanner, and other additional state
+
+    loop {
+        print!("> ");
+        let _ = io::stdout().flush();
+
+        let mut line = String::new();
+        let n = io::stdin().read_line(&mut line).unwrap_or_else(|err| {
+            eprintln!("Error: {err}");
+            0
+        });
+
+        if n == 0 {
+            break;
+        }
+
+        line.trim();
+        interpret(&line);
+    }
+}
 
 fn main() {
     let num_args = env::args().len();
@@ -20,8 +71,10 @@ fn main() {
         });
 
         process::exit(64);
+    } else if num_args == 2 {
+        let file = env::args().nth(1).unwrap();
+        run_file(&file);
+    } else {
+        run_repl();
     }
-
-    todo!();
-
 }
