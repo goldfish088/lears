5162d3c3b44c5dd7ef7c3b1f7cd8fdcd2aa361e0
Abraham <ae248abraham@gmail.com>
2026-01-26 11:07:44 +1100

    lox: [scanner] support for single-char tokens
    Added scan capability for single char tokens like '+', '-', etc.
    
    At this stage, the I do not know about lifetimes, so I think I
    am abusing the move mechanic to circumvent borrow-checking.

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,45 +1,150 @@
 use std::env;
-use std::process;
 use std::fs;
 use std::io::{self, Write};
-use std::vec;
+use std::process;
 
 #[allow(dead_code)]
 fn print_type<T>(_: &T) {
     println!("&type = {}", std::any::type_name::<&T>());
 }
 
-struct Token {
-
+#[derive(Debug, PartialEq)]
+enum Token {
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    COMMA,
+    EOF,
+    DOT,
+    MINUS,
+    SEMICOLON,
+    PLUS,
+    STAR,
 }
 
-fn scan_tokens() -> Vec<Token> {
-    Vec::new::<>()
+struct Scanner {
+    source: String,
+
+    line: usize,
+    lex_start_pos: usize,
+    lex_curr_pos: usize,
 }
 
-fn interpret(code: &str) {
-    println!("length = {}", code.len());
-    println!("code = {}", code);
-    todo!();
+struct ScanError {
+    line: usize,
+    message: String,
+}
+
+impl ScanError {
+    fn report(&self) {
+        eprintln!("[line {}] Error: {}", self.line, self.message);
+    }
+}
+
+impl Scanner {
+    fn can_scan(&self) -> bool {
+        self.lex_curr_pos < self.source.len()
+    }
+
+    fn next_char(&mut self) -> char {
+        assert!(self.can_scan());
+
+        let c = self
+            .source
+            .chars()
+            .nth(self.lex_curr_pos)
+            .unwrap_or_else(|| {
+                eprintln!("could not get char at pos {}", self.lex_curr_pos);
+                process::exit(65);
+            });
+
+        self.lex_curr_pos += 1;
+        c
+    }
+
+    fn scan_token(&mut self) -> Result<Token, ScanError> {
+        if !self.can_scan() {
+            return Ok(Token::EOF);
+        }
+
+        let c = self.next_char();
+        match c {
+            '(' => Ok(Token::LPAREN),
+            ')' => Ok(Token::RPAREN),
+            '{' => Ok(Token::LBRACE),
+            '}' => Ok(Token::RBRACE),
+            ',' => Ok(Token::COMMA),
+            '.' => Ok(Token::DOT),
+            '-' => Ok(Token::MINUS),
+            '+' => Ok(Token::PLUS),
+            ';' => Ok(Token::SEMICOLON),
+            '*' => Ok(Token::STAR),
+            _ => Err(ScanError {
+                line: self.line,
+                message: format!("Unexpected character '{}'.", c),
+            }),
+        }
+    }
+
+    fn scan_tokens(&mut self) -> Result<Vec<Token>, Vec<ScanError>> {
+        let mut tokens = Vec::<Token>::new();
+        let mut errors = Vec::<ScanError>::new();
+
+        loop {
+            self.lex_start_pos = self.lex_curr_pos;
+            match self.scan_token() {
+                Ok(token) => {
+                    tokens.push(token);
+                    if *tokens.last().unwrap() == Token::EOF {
+                        break;
+                    }
+                }
+                Err(error) => errors.push(error),
+            }
+        }
+
+        if errors.len() > 0 {
+            Err(errors)
+        } else {
+            Ok(tokens)
+        }
+    }
+}
+
+fn interpret(code: String) {
+    let mut scanner = Scanner {
+        source: code,
+        line: 1,
+        lex_curr_pos: 0,
+        lex_start_pos: 0,
+    };
+
+    match scanner.scan_tokens() {
+        Ok(tokens) => {
+            for token in tokens {
+                println!("{:#?}", token);
+            }
+        }
+        Err(errors) => {
+            for error in errors {
+                error.report();
+            }
+        }
+    }
 }
 
 fn run_file(path: &String) {
-    // if code.is_err() {
-    //     eprintln!("oops something went wrong");
-    //     process::exit(42);
-    // }
     let code = fs::read_to_string(path).unwrap_or_else(|_| {
         eprintln!("oops something went wrong");
-        process::exit(42);
+        String::new()
     });
 
     print_type(&code);
-    interpret(&code);
+    interpret(code);
 }
 
 fn run_repl() {
-    // init scanner, and other additional state
-
     loop {
         print!("> ");
         let _ = io::stdout().flush();
@@ -54,8 +159,8 @@ fn run_repl() {
             break;
         }
 
-        line.trim();
-        interpret(&line);
+        let _ = line.pop();
+        interpret(line);
     }
 }
 
@@ -65,10 +170,13 @@ fn main() {
         let fullpath = env::args().next().unwrap();
 
         // NOTE: only UNIX compatible...
-        println!("Usage: ./{} [script]", match fullpath.rfind('/') {
-            Some(i) => String::from(&fullpath[i+1..]),
-            _ => fullpath
-        });
+        println!(
+            "Usage: ./{} [script]",
+            match fullpath.rfind('/') {
+                Some(i) => String::from(&fullpath[i + 1..]),
+                _ => fullpath,
+            }
+        );
 
         process::exit(64);
     } else if num_args == 2 {
